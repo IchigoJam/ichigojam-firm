2dcb04799359e79f027f0201318f7f060819e0e3
Taisuke Fukuno <fukuno@jig.jp>
2025-04-01 09:17:58 +0900

    implemented USR

--- IchigoJam_MCX/source/usr.h
+++ IchigoJam_MCX/source/usr.h
@@ -1,7 +1,21 @@
 // Copyright 2014-2024 the IchigoJam authors. All rights reserved. MIT license.
 
-INLINE int IJB_usr(int ad, int n);
+inline int IJB_usr(int ad, int n);
 
-INLINE int IJB_usr(int ad, int n) {
-	return 0;
+extern uint64 __aeabi_uidiv(uint numerator, uint denominator);
+#define FUNC_DIVU __aeabi_uidiv
+
+inline int IJB_usr(int ad, int n) {
+	if (ad >= OFFSET_RAMROM && ad < SIZE_RAM + OFFSET_RAMROM) { // b10 limit:0x800 -> 0x1000
+		int (*f)(int, void*, void*, void*) = (void*)(ram + ad - (OFFSET_RAMROM - 1));
+		void* mem = (void*)((uint)ram - OFFSET_RAMROM); // R1
+		void* rom = (void*)CHAR_PATTERN; // R2
+		void* fdiv = (void*)FUNC_DIVU; // R3
+		_g.err = ERR_SEGMENTATION_FAULT;
+		n = f(n, mem, rom, fdiv); // 1.2.2b31
+		_g.err = 0;
+	} else {
+		command_error(ERR_ILLEGAL_ARGUMENT);
+	}
+	return n;
 }
